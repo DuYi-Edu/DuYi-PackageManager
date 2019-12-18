7738b92ba710a8ebefc071eb38c5cfc69d6bd2c2
yuanjin <yuanjinmoke@gmail.com>
2019-12-18 16:42:11 +0800

    2-9. 发布包

---
+++ 2. npm/2-9. 发布包/another/index.js
@@ -0,0 +1,3 @@
+var func = require("testabcd1234")
+var newArr = func([45,7,0,false,"",21])
+console.log(newArr)
---
+++ 2. npm/2-9. 发布包/test12345/index.js
@@ -0,0 +1,5 @@
+var _ = require("lodash");
+
+module.exports = function (arr) {
+    return _.compact(arr);
+}
